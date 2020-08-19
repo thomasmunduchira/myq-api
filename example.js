cf9f1a3b80984c88edb56390561b8b4bf2af1a62
Parnic <github@parnic.com>
2020-08-19 14:44:10 -0500

    More example cleanup
    This example was showing a feature of `getDevices()` that no longer exists. It was also chaining promises incorrectly with a block expecting the result of getDevices firing after a call to setDoorOpen. This cleans all of the example code up to run as-is after replacing email and password (and possibly also the `desiredDeviceName`).

--- example.js
+++ example.js
@@ -8,7 +8,7 @@ var delay = function delay(time) {
   });
 };
 
-var data = {};
+var device = {};
 console.log('Logging in.');
 account.login()
   .then(function (result) {
@@ -16,9 +16,8 @@ account.login()
     if (result.returnCode !== 0) {
       throw new Error('Login unsuccessful.');
     }
-    const deviceType = constants.allDeviceTypes.wifiGarageDoorOpener;
-    console.log(`\nGetting all ${deviceType} devices on account (check README for all possible types)`);
-    return account.getDevices(deviceType);
+    console.log(`\nGetting all devices on account`);
+    return account.getDevices();
   })
   .then(result => {
     console.log('Get Devices result: ', result);
@@ -28,34 +27,12 @@ account.login()
 
     console.log(result);
     const desiredDeviceName = 'Garage Door Opener';
-    const device = result.devices.find(device => device.name === desiredDeviceName);
+    device = result.devices.find(device => device.name === desiredDeviceName);
     if (!device) {
       throw new Error(`\nCould not find device with name: ${desiredDeviceName}`);
     }
 
     return account.setDoorOpen(device.serialNumber, false)
-  })
-  .then(function (result) {
-    console.log('getDevices result:', result);
-    if (result.returnCode !== 0) {
-      throw new Error('getDevices unsuccessful!');
-    }
-
-    var doors = result.devices;
-    if (doors.length === 0) {
-      throw new Error('No doors found!');
-    }
-    console.log('Devices:');
-    for (var i = 0; i < doors.length; i += 1) {
-      var door = doors[i];
-      console.log('Name: ' + door.name + ', State: ' + door.doorState);
-    }
-    return doors;
-  })
-  .then(function (doors) {
-    data.doors = doors;
-    console.log('\nClosing first door');
-    return account.setDoorOpen(doors[0].serialNumber, false);
   })
   .then(function (result) {
     console.log('setDoorOpen result:', result);
@@ -69,7 +46,7 @@ account.login()
   })
   .then(function () {
     console.log('\nGetting state of first door');
-    return account.getDoorState(data.doors[0].serialNumber);
+    return account.getDoorState(device.serialNumber);
   })
   .then(function (result) {
     console.log('getDoorState result:', result);
